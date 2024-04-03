17ed02cd029a9e73ce6d73ac2130ea5544dce871
Pritam Sharma <notpritamsharma@gmail.com>
2024-04-03 10:58:37 +0530

    Product API DOne

--- src/main/java/in/notpritam/projectservice/models/Category.java
+++ src/main/java/in/notpritam/projectservice/models/Category.java
@@ -12,5 +12,5 @@ import lombok.Setter;
 public class Category {
     private Long id;
     private String title;
-
+    private String description;
 }
--- src/main/java/in/notpritam/projectservice/services/FakeStoreProductService.java
+++ src/main/java/in/notpritam/projectservice/services/FakeStoreProductService.java
@@ -1,20 +1,44 @@
 package in.notpritam.projectservice.services;
 
 import in.notpritam.projectservice.dtos.FakeStoreProductDTO;
+import in.notpritam.projectservice.models.Category;
 import in.notpritam.projectservice.models.Product;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class FakeStoreProductService implements ProductService{
+
+    RestTemplate restTemplate;
     @Override
     public Product getProductById(Long id) {
 
         // get the product by using id from here
 
-        RestTemplate restTemplate = new RestTemplate();
+        FakeStoreProductDTO fakeStoreProductDTO = restTemplate.getForObject("https://fakestoreapi.com/products/"+id, FakeStoreProductDTO.class);
 
-        restTemplate.getForObject("https://fakestoreapi.com/products/"+id, FakeStoreProductDTO.class);
-        return null;
+        return mapFakeStoreProductDTOToProduct(fakeStoreProductDTO);
+    }
+
+    public Product mapFakeStoreProductDTOToProduct(FakeStoreProductDTO fakeStoreProductDTO) {
+        // Check if data is null
+        if (fakeStoreProductDTO == null) {
+            return null;
+        }
+
+        // Map data to Product object
+        Product product = new Product();
+        product.setId(fakeStoreProductDTO.getId());
+        product.setTitle(fakeStoreProductDTO.getTitle());
+        product.setPrice(fakeStoreProductDTO.getPrice());
+        product.setDescription(fakeStoreProductDTO.getDescription());
+        product.setImage(fakeStoreProductDTO.getImage());
+
+        // Map category
+        Category category = new Category();
+        category.setDescription(fakeStoreProductDTO.getCategory());
+        product.setCategory(category);
+
+        return product;
     }
 }
